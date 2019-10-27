4c072df805b4edb3f012ee7c5829a179df188c07
Hanna Lychauka <zuzga@tut.by>
2019-10-27 21:48:22 +0300

    changed permission for LectureCreate

--- lecture/views.py
+++ lecture/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, exceptions
 from .serializers import *
 from .models import Lecture
 from rest_framework.permissions import IsAuthenticated
@@ -7,10 +7,17 @@ from .permissions import *
 
 class LectureCreateView(generics.CreateAPIView):
     serializer_class = LectureCreateSerializer
-    permission_classes = (IsAuthenticated, IsOwnerOrTeacher)
+    permission_classes = (IsAuthenticated, )
 
     def perform_create(self, serializer):
-        serializer.save(course=Course.objects.get(id=self.request.session['course']))
+        course = Course.objects.get(id=self.request.session['course'])
+        is_course_owner = self.request.user == course.course_owner
+        is_teacher = self.request.user in course.teachers.all()
+
+        if is_course_owner or is_teacher:
+            serializer.save(course=course)
+        else:
+            raise exceptions.PermissionDenied()
 
 
 class LectureDetailView(generics.RetrieveUpdateDestroyAPIView):
