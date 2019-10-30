fe3ca13c3aaa79af86653fe37c490ed56ff3586a
Hanna Lychauka <zuzga@tut.by>
2019-10-30 23:19:41 +0300

    Changed Login View

--- user_auth/serializers.py
+++ user_auth/serializers.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
 from .models import User
-from django.contrib.auth import authenticate
-from rest_framework.serializers import (EmailField, CharField, ValidationError)
+from django.contrib.auth import authenticate, login
+from rest_framework.serializers import CharField, ValidationError
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'password', 'email')
@@ -18,14 +17,16 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
 class UserLoginSerializer(serializers.ModelSerializer):
     username = CharField()
+
     class Meta:
         model = User
         fields = ('username', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-    def validate(self, attrs):
-        user = authenticate(**attrs)
+    def create(self, validated_data):
+        user = authenticate(**validated_data)
         if user is not None:
-            return attrs
+            login(self.context['request'], user=user)
+            return user
         else:
             raise ValidationError('Incorrect credentials')
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,23 +1,10 @@
 from rest_framework import generics
 from .serializers import UserCreateSerializer, UserLoginSerializer
-from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
-from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
 
 
 class UserCreateView(generics.CreateAPIView):
     serializer_class = UserCreateSerializer
 
 
-class UserLoginView(APIView):
-    permission_classes = (AllowAny, )
+class UserLoginView(generics.CreateAPIView):
     serializer_class = UserLoginSerializer
-
-    def post(self, request):
-        data = request.data
-        serializer = UserLoginSerializer(data=data)
-        if serializer.is_valid(raise_exception=True):
-            new_data = serializer.data
-            return Response(new_data, status=HTTP_200_OK)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
